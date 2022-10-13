6d32e08aed48f8a1abda70465120b3a7543a85fa
Akash-Preet <preetinakash@gmail.com>
2022-10-13 14:47:55 +0530

    Query and Mutation from databse

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,22 +1,36 @@
+import { ObjectId } from "mongodb";
+import { Database, Listing } from "./../lib/types";
 import { IResolvers } from "@graphql-tools/utils";
-import { listings } from "../listings";
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
+      return await db.listings.find({}).toArray();
     },
   },
 
   Mutation: {
-    deleteListing: (_root_: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      const deleteRes = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
+
+      if (!deleteRes.value) {
+        throw new Error("failed to delete listing");
       }
 
-      throw new Error("failed to deleted listing");
+      return deleteRes.value;
     },
+  },
+  Listing: {
+    id: (listing: Listing): string => listing._id.toString(),
   },
 };
